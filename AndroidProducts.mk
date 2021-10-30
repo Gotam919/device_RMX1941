cefafa044534293b5309027470337abad9aaefea
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-10-30 23:00:49 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1941.mk
+    $(LOCAL_DIR)/arrow_RMX1941.mk
 	
 COMMON_LUNCH_CHOICES := \
-        lineage_RMX1941-userdebug
+        arrow_RMX1941-userdebug
         
         
