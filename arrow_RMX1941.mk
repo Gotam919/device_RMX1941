ed0d737c07f393ed83cb42ac4e2fd668cb0d3aea
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-11-01 12:19:06 +0530

    Update arrow_RMX1941.mk

--- arrow_RMX1941.mk
+++ arrow_RMX1941.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common arrow stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from RMX1941 device makefile
 $(call inherit-product, device/realme/RMX1941/device.mk)
