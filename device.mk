c080194d50a1956ea3566cde5634c379060d2a66
Gotam919 <82812329+Gotam919@users.noreply.github.com>
2021-11-16 14:10:10 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -26,8 +26,12 @@ $(call inherit-product, vendor/realme/RMX1941/RMX1941-vendor.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-
-
+#DPI
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.sf.lcd_density=423	
+	
+# Parts
+$(call inherit-product-if-exists, packages/apps/RealmeParts/parts.mk)
 
 PRODUCT_SHIPPING_API_LEVEL := 28
 
